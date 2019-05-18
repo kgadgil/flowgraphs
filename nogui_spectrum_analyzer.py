de5c38590480c16fa5bedf8f338b924425e9da07
human <krightangle@gmail.com>
2019-05-18 19:57:33 -0400

    stashing

--- nogui_spectrum_analyzer.py
+++ nogui_spectrum_analyzer.py
@@ -3,12 +3,13 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Mon Mar 11 14:18:51 2019
+# Generated: Tue Mar 19 14:58:59 2019
 ##################################################
 
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import uhd
+from gnuradio.ctrlport.monitor import *
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
@@ -49,6 +50,7 @@ class top_block(gr.top_block):
         self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
         self.uhd_usrp_sink_0.set_center_freq(0, 0)
         self.uhd_usrp_sink_0.set_gain(0, 0)
+        self.blocks_ctrlport_monitor_performance_0 = not True or monitor("gr-perf-monitorx")
 
 
 
@@ -76,6 +78,7 @@ def main(top_block_cls=top_block, options=None):
 
     tb = top_block_cls()
     tb.start(10)
+    (tb.blocks_ctrlport_monitor_performance_0).start()
     tb.wait()
 
 
